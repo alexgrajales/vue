812ad35bc53a90d41456ae0f5dedbff72eba7dbb
alexander grajales <alex-12-04@hotmail.com>
2018-10-15 08:32:39 -0500

    se cambia contenido

--- src/components/site/About.vue
+++ src/components/site/About.vue
@@ -6,10 +6,12 @@
     <h4>Autores</h4>    
     
     <h4>Alexander Grajales Vanegas</h4>
-    <p>Analizar la relación entre los elementos de Alice y Java, mediante los diferentes diagramas UML, estableciendo niveles de comparación y relación que permitan el diseño de los algoritmos</p>
+    <p>Estudiante politecnico jaime isaza cadavid</p>
+    <p>Analista Programador Agrolevels S.A.S</p>
     
     <h4>Juan Diego Ortiz</h4>
-    <p>Analizar la relación entre los elementos de Alice y Java, mediante los diferentes diagramas UML, estableciendo niveles de comparación y relación que permitan el diseño de los algoritmos</p>
+    <p>Estudiante politecnico jaime isaza cadavid</p>
+    <p>Analista Programador Ilimitada S.A.S</p>
     
     <h4>LUIS FERNANDO GONZÁLEZ ALVARÁN </h4>
     <p>Analizar la relación entre los elementos de Alice y Java, mediante los diferentes diagramas UML, estableciendo niveles de comparación y relación que permitan el diseño de los algoritmos</p>
